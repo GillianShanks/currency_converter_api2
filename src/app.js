128902f8d0def3ce6505d6f90af890d61e634271
Gillian Shanks <GillianShanks88@gmail.com>
2019-08-27 23:02:14 +0100

    tidies up the switching from/to and refactors code

--- src/app.js
+++ src/app.js
@@ -6,40 +6,23 @@ document.addEventListener('DOMContentLoaded', () => {
     data: {
       rates: null,
       base: null,
-      fromTo: 'from',
       selectedRate1: "",
       selectedRate2: "",
       input: 0,
     },
     computed: {
       output: function(){
-        if (this.selectedRate1===1){
-          if (this.fromTo==='from') {
-            return (this.input * this.selectedRate2).toFixed(2);
-          } else {
-            return (this.input / this.selectedRate2).toFixed(2);
-          }
-        } else {
-          if (this.fromTo === 'from') {
-            const toEuro = (this.input / this.selectedRate1).toFixed(2);
-            return (toEuro * this.selectedRate2).toFixed(2);
-          } else {
-            const fromEuro = (this.input * this.selectedRate1).toFixed(2);
-            return (fromEuro * this.selectedRate2).toFixed(2);
-          }
-        }
+        const toEuro = (this.input / this.selectedRate1).toFixed(2);
+        return (toEuro * this.selectedRate2).toFixed(2);
 
       },
       selectedCurrency: function(){
-        return Object.keys(this.rates).find(key => this.rates[key] === this.selectedRate2)
-      },
-      toFrom: function(){
-        if (this.fromTo==='from'){
-          return 'to';
+        if (this.selectedRate2===1){
+          return this.base;
         } else {
-          return 'from';
+          return Object.keys(this.rates).find(key => this.rates[key] === this.selectedRate2);
         }
-      }
+      },
     },
     mounted(){
       this.fetchRates();
