ceb3a51353af79320841114c66dbc7362d69cb86
Gillian Shanks <GillianShanks88@gmail.com>
2019-08-27 22:53:39 +0100

    adds output functionality to convert toEuro then to the selectedRate2, and fromEuro to the selectedRate2.

--- src/app.js
+++ src/app.js
@@ -7,20 +7,31 @@ document.addEventListener('DOMContentLoaded', () => {
       rates: null,
       base: null,
       fromTo: 'from',
-      selectedRate: 0,
+      selectedRate1: "",
+      selectedRate2: "",
       input: 0,
     },
     computed: {
       output: function(){
-        if (this.fromTo==='from') {
-          return (this.input * this.selectedRate).toFixed(2);
-        } else if (this.selectedRate){
-          return (this.input / this.selectedRate).toFixed(2);
+        if (this.selectedRate1===1){
+          if (this.fromTo==='from') {
+            return (this.input * this.selectedRate2).toFixed(2);
+          } else {
+            return (this.input / this.selectedRate2).toFixed(2);
+          }
+        } else {
+          if (this.fromTo === 'from') {
+            const toEuro = (this.input / this.selectedRate1).toFixed(2);
+            return (toEuro * this.selectedRate2).toFixed(2);
+          } else {
+            const fromEuro = (this.input * this.selectedRate1).toFixed(2);
+            return (fromEuro * this.selectedRate2).toFixed(2);
+          }
         }
 
       },
       selectedCurrency: function(){
-        return Object.keys(this.rates).find(key => this.rates[key] === this.selectedRate)
+        return Object.keys(this.rates).find(key => this.rates[key] === this.selectedRate2)
       },
       toFrom: function(){
         if (this.fromTo==='from'){
@@ -41,7 +52,8 @@ document.addEventListener('DOMContentLoaded', () => {
           this.rates=data.rates;
           this.base=data.base;
         });
-      }
+      },
+
     }
   });
 })
